05ecc596f6f203ef99f86c70c75c6f073012814c
nomadinsunda <nomadinsunda@gamil.com>
2025-06-29 13:33:59 +0900

    1st commit

---
+++ src/main/java/mypkg/HelloServlet.java
@@ -0,0 +1,43 @@
+package mypkg;
+
+import java.io.*;
+import jakarta.servlet.*;             // Tomcat 10
+import jakarta.servlet.http.*;        // Tomcat 10
+import jakarta.servlet.annotation.*;  // Tomcat 10
+//import javax.servlet.*;             // Tomcat 9
+//import javax.servlet.http.*;        // Tomcat 9
+//import javax.servlet.annotation.*;  // Tomcat 9
+
+// Configure the request URL for this servlet (Tomcat 7/Servlet 3.0 upwards)
+// @WebServlet("/sayhello")
+public class HelloServlet extends HttpServlet {
+   @Override
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+               throws IOException, ServletException {
+      // Set the response message's MIME type
+      response.setContentType("text/html;charset=UTF-8");
+      // Allocate a output writer to write the response message into the network socket
+      PrintWriter out = response.getWriter();
+ 
+      // Write the response message, in an HTML page
+      try {
+         out.println("<!DOCTYPE html>");
+         out.println("<html><head>");
+         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+         out.println("<title>Hello, World</title></head>");
+         out.println("<body>");
+         out.println("<h1>Hello, world!</h1>");  // says Hello
+         // Echo client's request information
+         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
+         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
+         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
+         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
+         // Generate a random number upon each request
+         out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
+         out.println("</body>");
+         out.println("</html>");
+      } finally {
+         out.close();  // Always close the output writer
+      }
+   }
+}
